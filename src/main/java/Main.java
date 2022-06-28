ea682430aa3b25a2d7d2d676ae62292862824c5c
Kir <87722197+Machnev999@users.noreply.github.com>
2022-06-28 18:30:23 +0300

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        BonusService service = new BonusService();
+        //void - не возвращает никуда данные
+        BonusService service = new BonusService();// вызываем целевой объект БонусСервиса
+        int bonus = service.calcBonus(500, true);//создаем переменную инт (коробка) и просим узнать из бонусСервиса в методе калкСервис что получим при оплате в 500р зарег пол.зоватля
 
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
+        System.out.println("БОНУС СОСТАВЛЯЕТ " + bonus);
 
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        // если true - то PASS
-        // если false - то FAIL
-        boolean passed = expected == actual;
-
-        // выводим результат
-        System.out.println(passed);
     }
+
+
 }
